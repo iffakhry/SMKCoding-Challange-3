7bf00880de576197a15455ddce930fbfbfc2555f
Remap Development <mohammadrizkyk@gmail.com>
2020-05-17 10:45:29 +0700

    init

---
+++ app/src/main/java/id/smkcoding/smkcodingchallenge2/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package id.smkcoding.smkcodingchallenge2
+
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    private val JUMLAH_MENU = 3
+
+    override fun createFragment(position: Int): Fragment {
+//        if (position == 0) {
+//            return GithubFragment()
+//        } else {
+//            return GithubFragment()
+//        }
+        when (position) {
+            0 -> { return MyFriendsFragment() }
+            1 -> { return GithubFragment() }
+            2 -> { return ProfilFragment() }
+            else -> {
+                return GithubFragment()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return JUMLAH_MENU
+    }
+}
